f884dff6a447ead68569d3fc75a0b8762ec55d82
11m245 <sivaraj2siva@gmail.com>
2023-04-29 07:39:48 +0530

    login routes

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+import { MongoClient } from "mongodb";
+import cors from "cors";
+import userRouter from "./routes/user.routes.js";
+import { Server } from "socket.io";
+import http from "http";
+
+const app = express();
+app.use(cors());
+
+export const client = new MongoClient(process.env.MONGO_URL);
+await client.connect();
+console.log("mongo connected");
+
+// const expressServer = app.listen(process.env.PORT, () =>
+//   console.log("exp server started in PORT", process.env.PORT)
+// );
+
+// const io = new Server(expressServer);
+
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: process.env.CLIENT_API },
+});
+
+httpServer.listen(process.env.PORT, () =>
+  console.log("http server started in PORT", process.env.PORT)
+);
+
+app.get("/", (request, response) => {
+  response.send("Welcome to Chat API");
+});
+
+io.on("connection", (socket) => {
+  // save every connecting in online users
+  console.log(socket.id);
+  socket.on("connection", () => {
+    console.log("socket connection");
+  });
+  socket.on("userConnection", function (data) {
+    console.log(data);
+  });
+  socket.on("disconnect", function () {
+    console.log("A user disconnected");
+  });
+  socket.on("connection", function () {
+    console.log("A user disconnected");
+  });
+  socket.on("new-user", () => {
+    io.emit("new-user", "hi, new user connected");
+  });
+  io.emit("check", "checking content");
+  socket.on("connect_error", (error) => {
+    console.log("connection error", error);
+  });
+});
+
+app.use("/user", userRouter);
